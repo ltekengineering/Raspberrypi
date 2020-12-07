2d68fbabda812ad529db25dd7bbba8fe7326c764
Lawrence Khan <lkhan9865@gmail.com>
2020-12-07 16:34:22 -0500

    Python scripts
    Email class

---
+++ pythonscripts/transmission.py
@@ -0,0 +1,29 @@
+class Email:
+    import smtplib,configparser
+    import email.utils
+    from email.mime.text import MIMEText
+
+    def __init__(self,serverType):
+        self.ini = self.configparser.ConfigParser()
+        self.ini.read("config.ini")
+        self.serverType = serverType
+
+    def sendEmail(self, From, To, Subject, Message):
+        #setup server connection        
+        if self.serverType == "gmail":
+            server = self.smtplib.SMTP( self.ini.get("gmail","server"), int(self.ini.get("gmail","port")) )
+            server.ehlo()
+            server.starttls()
+            server.login(self.ini.get("gmail","username"),self.ini.get("gmail","password") )        
+        else:
+            server = self.smtplib.SMTP("localhost")
+
+        #create the message
+
+        msg = self.MIMEText(Message,"plain")    
+        msg['Subject'] = Subject
+        msg["From"] = From
+        msg["To"] = To
+
+        server.sendmail(From, [To],msg.as_string())
+        server.quit()
